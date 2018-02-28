ffbea3f62d68b8977832cb84d01066d96893d67a
Nitin Kshatriya <nitinkshatriya29@gmail.com>
2018-02-28 12:11:08 +0530

    check_link() and link_maker() added

--- crawler_selenium.py
+++ crawler_selenium.py
@@ -3,12 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from pymongo import MongoClient
 import uuid
+import re
 
 def visit(get_link):
 	#options = Options()
 	#options.add_argument("--headless")
 	#browser = webdriver.Firefox(firefox_options=options)
 	browser = webdriver.Firefox()
+	#browser = webdriver.Chrome('C:\Users\Val\Anaconda2\selenium\webdriver\chromedriver.exe')
+	print(get_link)
 	try:
 		browser.get(get_link)
 		soup = BeautifulSoup(browser.page_source, "lxml")
@@ -21,7 +24,27 @@ def visit(get_link):
 def create_unique_object_id():
     unique_object_id = "RA_{uuid}".format(uuid=uuid.uuid4())
     return unique_object_id
- 
+
+# To trackle links like /product.html, /home.html
+def check_link(link, site):
+    if site in link:
+        return 'In'
+    elif link.startswith('http'):
+        return 'Out'
+    elif len(link) < 2:
+    	return 'Out'
+    else: return 'Half'
+
+
+def link_maker(half_link, site):
+    """Manages Relative links"""
+    if half_link.startswith('/'):
+        link = site + half_link[1:]
+    else:
+        link = site + half_link
+    return link
+
+
 valiance = MongoClient('192.168.2.152:27000')
 db = valiance['TCR']
 collections = db.company_url.find_one({})
@@ -29,10 +52,11 @@ collections = db.company_url.find_one({})
 for site in collections['url']:
 	if site.startswith('http') is False:
 		site = 'http://'+site
-	if db.test.find({'url':site}).count():
-		print(site)
+	if site.endswith('/') is False:
+		site = site+'/'
+	if db.test.find({'url':site}):
+		print('Already done for '+site)
 		continue
-	temp = site.split(':')
 	depth = 2
 	sites = set()
 	visited = set()
@@ -42,26 +66,32 @@ for site in collections['url']:
 	for i in range(depth):
 	    next = set()
 	    for links in sites:
-	    	print(links)
 	        soup = visit(links)
 	        if soup is not None:
 	            if i is 0:
-	                dict_data = {"id":id, "url":links, "title":str(soup.title.text), "data":str(soup), "section":[]}
-	                db.test.insert_one(dict_data)
+	                try:
+	                    dict_data = {"id":id, "url":links, "title":str(soup.title.text), "data":str(soup), "section":[]}
+	                    db.test.insert_one(dict_data)
+	                except:
+	                    dict_data = {"id":id, "url":links, "title":None, "data":str(soup), "section":[]}
+	                    db.test.insert_one(dict_data)
 	            else:
 	                try:
 	                    dict_data = {"url":links, "title":str(soup.title.text), "data":str(soup)}
 	                    db.test.update_one({"url":site}, {"$push":{"section": dict_data}})
 	                except:
-	                	print('Error')
+	                    dict_data = {"id":id, "url":links, "title":None, "data":str(soup), "section":[]}
+	                    db.test.insert_one(dict_data)
 	            for link in soup.find_all('a', href = True):
-	                if temp[1] in link['href'] or link['href'].startswith('/'):
-	                    if link['href'].startswith('/') and len(link['href']) > 1:
-	                        link['href'] = site+link['href'][1:]
+	                link_status = check_link(link['href'], site)
+	                if link_status is 'Out':
+	                    continue
+	                elif link_status is 'Half':
+	                    link['href'] = link_maker(link['href'], site)
 	                if link['href'] not in visited:
 	                    #print(link['href'])
 	                    visited.add(link['href'])
 	                    next.add(link['href'])
 	    print("Crawling done for "+links)
 	    sites = next
-	print("Over for "+site+" in collections")
+	print("Site Complete: "+site)
